20d7b492b0231731bee2ff5d0f31cd36100f148e
Dina94 <d_ina094@yahoo.com>
2016-11-06 21:37:57 +0200

    Add files via upload

---
+++ Activity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Subject
+{
+    class Activity
+    {
+        public String name { get; internal set; }
+
+        internal Activity(String denumire)
+        {
+            name = denumire;
+        }
+
+        public override string ToString()
+        {
+            return name.ToString();
+        }
+    }
+}
--- Student.cs
+++ Student.cs
@@ -1,75 +1,29 @@
-﻿using System;
+﻿using Models.Generics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace WebApplication3
+namespace Models.Student
 {
+    //Entity
     public class Student
     {
-        string[] nume;
-        string[] prenume;
-        string[] cnp;
-        string[] adresa;
-        float nota1, nota2, nota_exam;
-        public Student(string[] n, string[] p, string[] c, string[] adr, float n1,float n2, float n_e)
+        public RegistrationNumber RegNumber { get; internal set; }
+        public PlainText Name { get; internal set; }
+        public Credits Credits { get; internal set; }
+
+        public Student(RegistrationNumber regNumber, PlainText name)
         {
-            n = nume;
-            p = prenume;
-            c = cnp;
-            adr=adresa;
-            n1=nota1;
-            n2 = nota2;
-            n_e = nota_exam;
+            RegNumber = regNumber;
+            Name = name;
         }
-        public string _nume
+
+        public Student(RegistrationNumber regNumber, PlainText name, Credits credits)
+            : this(regNumber, name)
         {
-
-
-            get { return _nume; }
-            set { _nume = value; }
-        }
-        public string _prenume
-        {
-
-
-            get { return _prenume; }
-            set { _prenume = value; }
-        }
-        public string _cnp
-        {
-
-
-            get { return _cnp; }
-            set { _cnp = value; }
-        }
-        public string _adresa
-        {
-
-
-            get { return _adresa; }
-            set { _adresa = value; }
-        }
-        public float _nota1
-        {
-
-
-            get { return _nota1; }
-            set { _nota1 = value; }
-        }
-        public string _nota2
-        {
-
-
-            get { return _nota2; }
-            set { _nota2 = value; }
-        }
-        public string _nota_exam
-        {
-
-
-            get { return _nota_exam; }
-            set { _nota_exam = value; }
+            Credits = credits;
         }
     }
 }
